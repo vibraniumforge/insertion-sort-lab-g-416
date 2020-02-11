c9f7ab0df31aecf24726c5f70baaaf42a7ebc40a
vibraniumforge <gregm9999@gmail.com>
2020-02-10 23:15:02 -0800

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-function findMinAndRemove(array){
-
+function findMinAndRemove(array) {
+  let minIndex = 0;
+  let currentMinimumValue = array[0];
+  for (let i in array) {
+    if (array[i] < currentMinimumValue) {
+      minIndex = i;
+      currentMinimumValue = array[i];
+    }
+  }
+  array.splice(minIndex, 1);
+  return currentMinimumValue;
 }
 
-function insertionSort(array){
-
+function insertionSort(array) {
+  let sorted = [];
+  let min;
+  while (array.length > 0) {
+    min = findMinAndRemove(array);
+    sorted.push(min);
+  }
+  return sorted;
 }
